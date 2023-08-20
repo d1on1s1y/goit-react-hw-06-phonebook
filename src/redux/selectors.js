45e9eb216091182f38c8b6d470c99f7e895db01e
d1on1s1y <dionisiy.god.of.wine@gmail.com>
2023-08-20 11:53:39 +0300

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,7 +1,10 @@
 export const selectFilteredContact = state => {
-    return state.contacts.contacts.filter(contact => {
-      return contact.name
-        .toLowerCase()
-        .includes(state.contacts.filter.toLowerCase());
-    });
-  };
+  if (!state.contacts.filter) {
+    return state.contacts.contacts;
+  }
+  return state.contacts.contacts.filter(contact => {
+    return contact.name
+      .toLowerCase()
+      .includes(state.contacts.filter.toLowerCase());
+  });
+};
